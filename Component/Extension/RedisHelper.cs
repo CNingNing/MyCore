6094556e51421d0a7876a64c9cd33161e292d84c
CNingNing <1731156291@qq.com>
2019-07-18 18:01:18 +0800

    测试控制台。可以提交进行类型的测试

---
+++ Component/Extension/RedisHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using StackExchange.Redis;
+namespace Component.Extension
+{
+    public  class RedisHelper
+    {
+
+        private static readonly string con = "127.0.0.1:6379";
+        
+
+
+    }
+}
